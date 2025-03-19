b2f5c7de2dbbac2db5fa77aca156e6bcea351362
edson <edson@edson-HP-ProDesk-400-G6-Desktop-Mini-PC>
2025-03-19 13:32:45 -0700

    Versión 3 19-mar-2025 * Se incluye en main la recarga del TIMER0 * Se agrega secuencia para desbloquear y bloquear PPS

--- 36161-P4.X/main.c
+++ 36161-P4.X/main.c
@@ -41,8 +41,14 @@ void main(void) {
     {
     
         /* Wait for TMR0IF = 1 */
-        while ((PIR3 & 0x80) != 0x80)
+       while ((PIR3 & 0x80) != 0x80)
                 ;
+       
+       	/* Reload TMR0 with initial value (hex) */        
+        /* tCLK1 x counts  x prescaler = TMR0 counting time */
+        /* 1/64MHz * (65535-64535) * 1 = 15.625 us */
+        TMR0H = 0xFC;
+        TMR0L = 0x17;
         
         /* Clear TMR0IF bit */
         PIR3 &= 0x7F;
